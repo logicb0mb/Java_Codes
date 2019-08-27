17a8e3c346d7ffc86bd9418be144b1dc8cf66cf7
Shreyas Shukla <shreyas.shukla26@gmail.com>
2019-08-27 23:47:32 +0530

    MethodOverloading_v2

--- MethodOverloading/src/com/shreyasshukla/Main.java
+++ MethodOverloading/src/com/shreyasshukla/Main.java
@@ -3,6 +3,31 @@ package com.shreyasshukla;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double result = calcFeetAndInchesToCentimeters(15,9);
+        System.out.println(result);
+        double result2 = calcFeetAndInchesToCentimeters(189);
+        System.out.println(result2);
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if (feet>=0){
+            if (inches>=0 && inches<=12){
+                double centiMeters;
+                centiMeters = (inches * 2.54) + (feet * 12 * 2.54);
+                return centiMeters;
+            }
+            return -1;
+        }
+        return -1;
+    }
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if (inches >=0){
+            double feets = (int) inches/12;
+            double newInches =  inches - (feets * 12);
+//            System.out.println("feets:" + feets);
+//            System.out.println("newInches:" + newInches);
+            return calcFeetAndInchesToCentimeters(feets,newInches);
+        }
+        return -1;
     }
 }
