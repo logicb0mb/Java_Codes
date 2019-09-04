fbbb5cfa33abc730872be87582ed94f3f32d2cfb
Shreyas Shukla <shreyas.shukla26@gmail.com>
2019-09-04 22:28:39 +0530

    Sum3And5Challenge_v1

---
+++ Sum3And5Challenge/src/com/shreyasshukla/Main.java
@@ -0,0 +1,19 @@
+package com.shreyasshukla;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0, count =0;
+        for(int i =1 ;i <= 1000 ; i++) {
+            if( (i%3 ==0) && (i%5 == 0)){
+                sum += i;
+                count++;
+                System.out.println("NUmber : " + i);
+                if(count == 5) {
+                    break;
+                }
+            }
+        }
+        System.out.println("SUM: "+ sum);
+    }
+}
